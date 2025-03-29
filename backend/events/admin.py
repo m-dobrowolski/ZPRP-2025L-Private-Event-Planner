f09c6e4e652dc9672cad149a0f8d4059debaa201
Michal <michal.golawski.mg@gmail.com>
2025-03-29 19:15:49 +0100

    #8 Registered Event and Participant models on admin page

--- backend/events/admin.py
+++ backend/events/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Event, Participant
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id', 'uuid', 'name', 'location', 'start_datetime',
+                    'end_datetime', 'organizer_email')
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'event', 'name', 'password_hash', 'email', 'is_admin')
+
