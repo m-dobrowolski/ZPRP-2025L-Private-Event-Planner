7db41dc87ce9f864416ec7f611fada061c8f6836
Michal <michal.golawski.mg@gmail.com>
2025-04-08 17:46:48 +0200

    #2 Refactored urls patterns so they were more consistent

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -4,15 +4,15 @@ from events import views
 app_name = 'events'
 
 urlpatterns = [
-    path('events/', views.EventCreate.as_view(), name='event-list'),
+    path('event/create/', views.EventCreate.as_view(), name='event-create'),
     path('event/<uuid:uuid>/<uuid:edit_uuid>/', views.EventAdminDetail.as_view(), name='event-admin-detail'),
     path('event/<uuid:uuid>/', views.EventDetail.as_view(), name='event-detail'),
-    path('invitations/', views.InvitationCreate.as_view(), name='invitation-list'),
-    path('invitations/accept/', views.InvitationAccept.as_view(), name='accept-invitation'),
-    path('invitation/<uuid:uuid>/<uuid:edit_uuid>/', views.InvitationDelete.as_view(), name='invitation-delete'),
-    path('personalized-invitations/', views.PersonalizedInvitationCreate.as_view(), name='personalized-invitation-list'),
-    path('personalized-invitations/accept/', views.PersonalizedInvitationAccept.as_view(), name='accept-personalized-invitation'),
-    path('personalized-invitation/<uuid:uuid>/<uuid:edit_uuid>/', views.PersonalizedInvitationDelete.as_view(), name='personalized-invitation-delete'),
-    path('participant/<uuid:uuid>/', views.ParticipantDetail.as_view(), name='participant-detail'),
+    path('invitation/create/', views.InvitationCreate.as_view(), name='invitation-create'),
+    path('invitation/accept/', views.InvitationAccept.as_view(), name='invitation-accept'),
+    path('invitation/delete/<uuid:uuid>/<uuid:edit_uuid>/', views.InvitationDelete.as_view(), name='invitation-delete'),
+    path('personalized-invitation/create/', views.PersonalizedInvitationCreate.as_view(), name='personalized-invitation-create'),
+    path('personalized-invitation/accept/', views.PersonalizedInvitationAccept.as_view(), name='personalized-invitation-accept'),
+    path('personalized-invitation/delete/<uuid:uuid>/<uuid:edit_uuid>/', views.PersonalizedInvitationDelete.as_view(), name='personalized-invitation-delete'),
+    path('participant/<uuid:uuid>/', views.LeaveEvent.as_view(), name='leave-event'),
     path('participant/<int:id>/<uuid:edit_uuid>/', views.DeleteParticipantAsAdmin.as_view(), name='delete-participant-admin'),
 ]
